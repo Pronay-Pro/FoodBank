8de1a7495cdc56d80f4732e40456c553441b8747
Pronay Saha <pronaysaha788@gmail.com>
2021-02-07 18:38:15 +0600

    foodbank

---
+++ script.js
@@ -0,0 +1,57 @@
+const search = document.getElementById('search');
+search.addEventListener('click', function () {
+    const foodInput = document.getElementById('foodInput').value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${ foodInput }`)
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById('foodDisplay').innerHTML = "";
+            document.getElementById('foodInfo').innerHTML = ' ';
+            const foodDisplay = document.getElementById('foodDisplay');
+            data.meals.forEach(foodie => {
+                const food = document.createElement('div')
+                food.innerHTML = `
+            <img src="${ foodie.strMealThumb }" onClick="aboutFood(${ foodie.idMeal })">
+            <h1 onClick="aboutFood(${ foodie.idMeal })" >${ foodie.strMeal }</h1>
+            `;
+                food.className = "card";
+                foodDisplay.appendChild(food);
+            });
+        })
+        .catch(() => {
+            document.getElementById('foodDisplay').innerHTML = "";
+            document.getElementById('foodInfo').innerHTML = ' ';
+            const foodDisplay = document.getElementById('foodDisplay');
+            const alert = document.createElement('div') 
+            alert.innerHTML =`
+            <img class="alertImage" src="img/cooking.gif">
+            <h1 class="alertLine">Sorry can't find this meals...</h1>
+            `
+            foodDisplay.appendChild(alert);
+        })
+        document.getElementById('foodInput').value ="";
+})
+
+let aboutFood = foodInfo => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${ foodInfo }`)
+        .then(res => res.json())
+        .then(data => {
+            let foodDetails = document.getElementById('foodInfo');
+            document.getElementById('foodInfo').innerHTML = ' ';
+            document.getElementById('foodInfo').style.display = 'block';
+            let foodIngredients = document.createElement('div')
+            foodIngredients.innerHTML = `
+            <img class="foodImage" src="${ data.meals[0].strMealThumb }">
+            <h1>${ data.meals[0].strMeal }</h1>
+            <br>
+            <h1>Category: ${ data.meals[0].strCategory }</h1>
+            <br>
+            <h4><span class=icon>☑</span> ${ data.meals[0].strMeasure1 }</h4>
+            <h4><span class=icon>☑</span> ${ data.meals[0].strMeasure2 }</h4>
+            <h4><span class=icon>☑</span> ${ data.meals[0].strMeasure3 }</h4>
+            <h4><span class=icon>☑</span> ${ data.meals[0].strMeasure4 }</h4>
+            <h4><span class=icon>☑</span> ${ data.meals[0].strMeasure5 }</h4>
+            <h4><span class=icon>☑</span> ${ data.meals[0].strMeasure6 }</h4>
+            `;
+            foodDetails.appendChild(foodIngredients);
+        })
+}
